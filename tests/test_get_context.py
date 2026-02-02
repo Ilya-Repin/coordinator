3390933a4d0a7728476795aa31d6239925c6442f
Ilya Repin <ilya-r04@yandex.ru>
2026-02-02 04:04:09 +0000

    fix

--- tests/test_get_context.py
+++ tests/test_get_context.py
@@ -1,5 +1,4 @@
-# Start via `make test-debug` or `make test-release`
-
+import pytest
 
 async def test_get_context(service_client):
     response = await service_client.get('/admin/context')
--- tests/test_get_partition_map.py
+++ tests/test_get_partition_map.py
@@ -1,5 +1,4 @@
-# Start via `make test-debug` or `make test-release`
-
+import pytest
 
 async def test_get_partition_map(service_client):
     response = await service_client.get('/admin/partition_map')
